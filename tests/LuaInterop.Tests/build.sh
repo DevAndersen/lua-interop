a5911bef695f6e8c4cb7dace2dcac31d9bc1bccd
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2026-06-22 01:34:10 +0200

    Native build script

---
+++ tests/LuaInterop.Tests/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if ! command -v lua > /dev/null 2>&1
+then
+    echo "Error: Lua not found"
+    exit 1
+fi
+
+echo "Building native library to '$1'"
+dotnet publish ../../src/LuaInterop -r linux-x64 -c Release -o $1
