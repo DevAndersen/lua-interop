cb603048be0356a1aba3cfd4ed9ca90354989e4c
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2026-06-26 13:17:06 +0200

    Unix build script no longer ensures that Lua is accessible

--- tests/LuaInterop.Tests/build.sh
+++ tests/LuaInterop.Tests/build.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-if ! command -v lua > /dev/null 2>&1
-then
-    echo "Error: Lua not found"
-    exit 1
-fi
-
 echo "Building native library to '$1'"
 dotnet publish ../LuaInterop.Tests.Demo -r linux-x64 -c Release -o $1
