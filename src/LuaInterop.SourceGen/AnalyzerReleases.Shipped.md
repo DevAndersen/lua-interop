64fce3f720163636af6c40e4133f1ec8bb75454b
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2026-06-30 15:20:34 +0200

    Changed diagnostic levels

--- src/LuaInterop.SourceGen/AnalyzerReleases.Shipped.md
+++ src/LuaInterop.SourceGen/AnalyzerReleases.Shipped.md
@@ -5,8 +5,8 @@
 Rule ID | Category | Severity | Notes
 --------|----------|----------|-------
 LUA9999 | LUA | Warning | Debug diagnostic
-LUA0001 | LUA | Warning | Lua function must be static
-LUA0002 | LUA | Warning | Lua function must be either 'public' or 'internal'
-LUA0003 | LUA | Warning | Unsupported Lua function return type
-LUA0004 | LUA | Warning | Unsupported Lua function parameter type
-LUA0005 | LUA | Warning | Lua function contained in type not marked as either 'public' or 'internal'
+LUA0001 | LUA | Error | Lua function must be static
+LUA0002 | LUA | Error | Lua function must be either 'public' or 'internal'
+LUA0003 | LUA | Error | Unsupported Lua function return type
+LUA0004 | LUA | Error | Unsupported Lua function parameter type
+LUA0005 | LUA | Error | Lua function contained in type not marked as either 'public' or 'internal'
--- src/LuaInterop.SourceGen/Diagnostics.cs
+++ src/LuaInterop.SourceGen/Diagnostics.cs
@@ -19,7 +19,7 @@ internal static class Diagnostics
         title: "Lua function must be static",
         messageFormat: "Lua function must be static",
         category: _category,
-        DiagnosticSeverity.Warning,
+        DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
     public static readonly DiagnosticDescriptor MethodNotAccessible = new DiagnosticDescriptor(
@@ -27,7 +27,7 @@ internal static class Diagnostics
         title: "Lua function must be either 'public' or 'internal'",
         messageFormat: "Lua function must be either 'public' or 'internal'",
         category: _category,
-        DiagnosticSeverity.Warning,
+        DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
     public static readonly DiagnosticDescriptor ReturnTypeNotSupported = new DiagnosticDescriptor(
@@ -35,7 +35,7 @@ internal static class Diagnostics
         title: "Unsupported Lua function return type",
         messageFormat: "Unsupported Lua function return type '{0}'",
         category: _category,
-        DiagnosticSeverity.Warning,
+        DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
     public static readonly DiagnosticDescriptor ParameterTypeNotSupported = new DiagnosticDescriptor(
@@ -43,7 +43,7 @@ internal static class Diagnostics
         title: "Unsupported Lua function parameter type",
         messageFormat: "Unsupported Lua function parameter type '{0}'",
         category: _category,
-        DiagnosticSeverity.Warning,
+        DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
     public static readonly DiagnosticDescriptor ContainingTypeNotAccessible = new DiagnosticDescriptor(
@@ -51,6 +51,6 @@ internal static class Diagnostics
         title: "Lua function contained in type not marked as either 'public' or 'internal'",
         messageFormat: "Lua function contained in type {0} which is not marked as either 'public' or 'internal'",
         category: _category,
-        DiagnosticSeverity.Warning,
+        DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 }
