b154275a1674a0382c6ee1465502c712107591a6
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2026-07-07 11:10:24 +0200

    Reordered constants

--- src/LuaInterop.SourceGen/GeneratorConstants.cs
+++ src/LuaInterop.SourceGen/GeneratorConstants.cs
@@ -2,23 +2,26 @@
 
 internal static class GeneratorConstants
 {
-    public const string GeneratedCodeNamespace = "LuaInterop.Generated";
-    public const string LuaLibraryAttributeFullName = "LuaInterop.Attributes.LuaLibraryAttribute";
-    public const string LuaFunctionAttributeFullName = "LuaInterop.Attributes.LuaFunctionAttribute";
-    public const string LuaFunctionAttributeNameArgumentName = "FunctionName";
-    public const string LuaInteropHelperTypeFullName = "global::LuaInterop.LuaInteropHelper";
-    public const string LuaInteropHelperRegisterFunctionMethodName = "RegisterFunction";
-    public const string LuaInteropHelperCreateTableMethodName = "CreateTable";
-    public const string LuaReadHelperTypeFullName = "global::LuaInterop.LuaReadHelper";
-    public const string LuaPushHelperTypeFullName = "global::LuaInterop.LuaPushHelper";
+    // CoreLib
     public const string UnmanagedCallersOnlyAttributeFullName = "global::System.Runtime.InteropServices.UnmanagedCallersOnly";
     public const string GeneratedCodeAttributeAttributeFullName = "global::System.CodeDom.Compiler.GeneratedCodeAttribute";
+    public const string TypeMetadataNameDictionary2 = "System.Collections.Generic.IDictionary`2";
     public const string UnmanagedCallersOnlyAttributeEntryPointArgument = "EntryPoint";
-    public const string ReturnVariableName = "returnedValue";
+
+    // LuaInterop
+    public const string LuaLibraryAttributeFullName = "LuaInterop.Attributes.LuaLibraryAttribute";
+    public const string LuaFunctionAttributeFullName = "LuaInterop.Attributes.LuaFunctionAttribute";
+    public const string LuaReadHelperTypeFullName = "global::LuaInterop.LuaReadHelper";
+    public const string LuaPushHelperTypeFullName = "global::LuaInterop.LuaPushHelper";
+    public const string LuaInteropHelperTypeFullName = "global::LuaInterop.LuaInteropHelper";
+    public const string LuaInteropHelperRegisterFunctionMethodName = "RegisterFunction";
+    public const string LuaFunctionAttributeNameArgumentName = "FunctionName";
+
+    // Generator
+    public const string GeneratedCodeNamespace = "LuaInterop.Generated";
+    public const string LuaInteropHelperCreateTableMethodName = "CreateTable";
     public const string LuaOpenClassName = "LuaEntryPoint";
     public const string LuaOpenMethodName = "LuaOpen";
     public const string LuaStateVariableName = "luaState";
-
-    public const string TypeMetadataNameDictionary2 = "System.Collections.Generic.IDictionary`2";
-
+    public const string ReturnVariableName = "returnedValue";
 }
