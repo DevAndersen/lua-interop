109f4820c9419198ea431467da1029ece4473b0c
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2026-07-10 10:35:41 +0200

    Moved methods used by tests to TestMethods

--- tests/LuaInterop.Tests.Demo/EntryPoint.cs
+++ tests/LuaInterop.Tests.Demo/EntryPoint.cs
@@ -61,50 +61,50 @@ public static class EntryPoint
     //    return 4;
     //}
 
-    //[LuaFunction]
-    //public static int Addition(int a, int b)
-    //{
-    //    return a + b;
-    //}
+    [LuaFunction]
+    public static int Addition(int a, int b)
+    {
+        return a + b;
+    }
 
-    //[LuaFunction]
-    //public static bool ReadWriteBoolean(bool value)
-    //{
-    //    return value;
-    //}
+    [LuaFunction]
+    public static bool ReadWriteBoolean(bool value)
+    {
+        return value;
+    }
 
-    //[LuaFunction]
-    //public static bool? ReadWriteNullableBoolean(bool? value)
-    //{
-    //    return value;
-    //}
+    [LuaFunction]
+    public static bool? ReadWriteNullableBoolean(bool? value)
+    {
+        return value;
+    }
 
-    //[LuaFunction]
-    //public static string ReadWriteString(string value)
-    //{
-    //    return value;
-    //}
+    [LuaFunction]
+    public static string ReadWriteString(string value)
+    {
+        return value;
+    }
 
-    //[LuaFunction]
-    //public static string? ReadWriteNullableString(string? value)
-    //{
-    //    return value;
-    //}
+    [LuaFunction]
+    public static string? ReadWriteNullableString(string? value)
+    {
+        return value;
+    }
 
-    //[LuaFunction]
-    //public static string ReadStringWithNullCharacter()
-    //{
-    //    return "abc\0def";
-    //}
+    [LuaFunction]
+    public static string ReadStringWithNullCharacter()
+    {
+        return "abc\0def";
+    }
 
-    //[LuaFunction]
-    //public static Dictionary<int, string> WriteDictionary()
-    //{
-    //    return new Dictionary<int, string>
-    //    {
-    //        [1] = "A",
-    //        [2] = "B",
-    //        [3] = "C",
-    //    };
-    //}
+    [LuaFunction]
+    public static Dictionary<int, string> WriteDictionary()
+    {
+        return new Dictionary<int, string>
+        {
+            [1] = "A",
+            [2] = "B",
+            [3] = "C",
+        };
+    }
 }
---
+++ tests/LuaInterop.Tests.Demo/TestMethods.cs
@@ -0,0 +1,53 @@
+﻿using LuaInterop.Attributes;
+
+namespace LuaInterop.Tests.Demo;
+
+internal class TestMethods
+{
+    [LuaFunction]
+    public static int Addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    [LuaFunction]
+    public static bool ReadWriteBoolean(bool value)
+    {
+        return value;
+    }
+
+    [LuaFunction]
+    public static bool? ReadWriteNullableBoolean(bool? value)
+    {
+        return value;
+    }
+
+    [LuaFunction]
+    public static string ReadWriteString(string value)
+    {
+        return value;
+    }
+
+    [LuaFunction]
+    public static string? ReadWriteNullableString(string? value)
+    {
+        return value;
+    }
+
+    [LuaFunction]
+    public static string ReadStringWithNullCharacter()
+    {
+        return "abc\0def";
+    }
+
+    [LuaFunction]
+    public static Dictionary<int, string> WriteDictionary()
+    {
+        return new Dictionary<int, string>
+        {
+            [1] = "A",
+            [2] = "B",
+            [3] = "C",
+        };
+    }
+}
