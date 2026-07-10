b913a1c6a14e441f7d6b22c26116de8472641a21
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2026-07-10 12:02:19 +0200

    Documented attributes

--- src/LuaInterop/Attributes/LuaFunctionAttribute.cs
+++ src/LuaInterop/Attributes/LuaFunctionAttribute.cs
@@ -1,16 +1,39 @@
 ﻿namespace LuaInterop.Attributes;
 
+/// <summary>
+/// Marks this method as a Lua function, generating interop code so the method can be called from Lua.
+/// </summary>
+/// <remarks>
+/// The assembly must also be decorated with <see cref="LuaLibraryAttribute"/>.
+/// </remarks>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 public class LuaFunctionAttribute : Attribute
 {
+    /// <summary>
+    /// Set a custom name for the Lua function.
+    /// If not specified, the method's name will be used.
+    /// </summary>
     public string? FunctionName { get; init; }
 
+    /// <summary>
+    /// Marks the method as a manual interop method.
+    /// This opts out of source generated wrapper methods, while still exposing the method to Lua.
+    /// This method therefore has to fulfill the requirements for Lua interop,
+    /// and manually invoke the relevant interop helper methods to read/write data to and from Lua.
+    /// </summary>
     public bool ManualFunction { get; init; }
 
+    /// <summary>
+    /// Mark the method as a Lua function, exposing it to Lua as the name of the method.
+    /// </summary>
     public LuaFunctionAttribute()
     {
     }
 
+    /// <summary>
+    /// Mark the method as a Lua function, exposing it to Lua as <paramref name="functionName"/>.
+    /// </summary>
+    /// <param name="functionName"></param>
     public LuaFunctionAttribute(string functionName)
     {
         FunctionName = functionName;
--- src/LuaInterop/Attributes/LuaLibraryAttribute.cs
+++ src/LuaInterop/Attributes/LuaLibraryAttribute.cs
@@ -1,5 +1,8 @@
 ﻿namespace LuaInterop.Attributes;
 
+/// <summary>
+/// Enables generation of Lua interop code on the decorated assembly.
+/// </summary>
 [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
 public class LuaLibraryAttribute : Attribute
 {
